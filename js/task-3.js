9eaffb7dc410b6c23b56ca9033dfbd83a863d1d3
TarasApretov <134705215+TarasApretov@users.noreply.github.com>
2024-01-10 21:18:13 +0100

    W33

--- js/task-3.js
+++ js/task-3.js
@@ -1,5 +1,6 @@
 function getElementWidth(content, padding, border) { 
-    return (content + 2 * padding + 2 * border)
+    
+    return (Number.parseFloat(content) + 2 * Number.parseFloat(padding) + 2 * Number.parseFloat(border))
 };
 
 console.log(getElementWidth("50px", "8px", "4px")); // 74
